828504fb3e3d8d199f6665a9f7640665a655e902
Matthew Heck <matthewheck5@gmail.com>
2016-01-20 17:20:10 -0500

    eslint amazingness

---
+++ index.js
@@ -0,0 +1,26 @@
+module.exports = {
+	"parser": "babel-eslint",
+	"env": {
+		"node": true,
+		"mocha": true,
+		"browser": true,
+		"es6": true
+	},
+	"ecmaFeatures": {
+		"jsx": true
+	},
+	"settings": {
+		"ecmascript": 6,
+		"jsx": true
+	},
+	"globals": {},
+	"extends": [
+		"eslint-config-techchange/rules/possible-errors",
+		"eslint-config-techchange/rules/best-practices",
+		"eslint-config-techchange/rules/variables",
+		"eslint-config-techchange/rules/node",
+		"eslint-config-techchange/rules/es6",
+		"eslint-config-techchange/rules/strict",
+		"eslint-config-techchange/rules/style"
+	]
+};
