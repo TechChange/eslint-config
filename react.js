60c32159471da764eea32dcca7e5b779d6a60f7a
Matthew Heck <matthewheck5@gmail.com>
2016-01-21 13:18:11 -0500

    add react rules

--- react.js
+++ react.js
@@ -6,9 +6,19 @@ module.exports = {
 		"browser": true,
 		"es6": true
 	},
-	"settings": {
-		"ecmascript": 6
+	"ecmaFeatures": {
+		"jsx": true
 	},
+	"settings": {
+		"ecmascript": 6,
+		"jsx": true,
+		"react": {
+			"pragma": "React"
+		}
+	},
+	"plugins": [
+		"react"
+	],
 	"extends": [
 		"eslint-config-techchange/rules/possible-errors",
 		"eslint-config-techchange/rules/best-practices",
@@ -16,6 +26,7 @@ module.exports = {
 		"eslint-config-techchange/rules/node",
 		"eslint-config-techchange/rules/es6",
 		"eslint-config-techchange/rules/strict",
-		"eslint-config-techchange/rules/style"
+		"eslint-config-techchange/rules/style",
+		"eslint-config-techchange/rules/react"
 	]
 };
