4e77602e023294b37ddba07ab91f2036248a3fbc
Matthew Heck <matthewheck5@gmail.com>
2017-05-11 21:53:41 -0400

    update node rules

--- rules/node.js
+++ rules/node.js
@@ -7,13 +7,18 @@ module.exports = {
 		// Require error handling in callbacks using the "err or error" argument
 		"handle-callback-err": ["error", "^(err|error)$"],
 		// Warn if require() variables are declared with other non-require() variables
-		"no-mixed-requires": "warn",
+		"no-mixed-requires": "error",
 		// Disallow using the new keyword with require() function
 		"no-new-require": "error",
 		// Disallow string concatenation using __dirname or __filename as using path.join() and path.resolve() are safer across systems
 		"no-path-concat": "error",
+		// Disallow using process.env in node environments in favor of using config files
+		"no-process-env": "warn",
 		// Disallow the use of process.exit() as throw new Error("Message") allows other parts of the application to handle the error
 		"no-process-exit": "error",
+		// Disallow node.js modules
+		// This list is taken from http://eslint.org/docs/rules/no-restricted-modules
+		"no-restricted-modules": ["error", "assert","buffer","child_process","cluster","crypto","dgram","dns","domain","events","freelist","fs","http","https","module","net","os","path","punycode","querystring","readline","repl","smalloc","stream","string_decoder","sys","timers","tls","tracing","tty","url","util","vm","zlib"],
 		// Warn when using synchronous versions of modules where an asynchronous version exists
 		"no-sync": "warn"
 	}
