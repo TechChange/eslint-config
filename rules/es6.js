7687481bf56dd6d5e56485d2864379482cdd2d40
Matthew Heck <matthewheck5@gmail.com>
2017-05-11 21:53:32 -0400

    update es6 rules

--- rules/es6.js
+++ rules/es6.js
@@ -1,8 +1,9 @@
 module.exports = {
 	"rules": {
-		// DISABLED UNTIL MORE FLEXIBLE:
 		// Require braces around arrow body when required
-		"arrow-body-style": ["off", "as-needed"],
+		"arrow-body-style": ["error", "as-needed", {
+			"requireReturnForObjectLiteral": true
+		}],
 		// Require parens around arrow-functions
 		"arrow-parens": ["error", "always"],
 		// Require spaces before and after arrow-function declarations
@@ -11,31 +12,57 @@ module.exports = {
 		"constructor-super": "error",
 		// Enforce space before generator *, no space after (e.g. function *generator() {})
 		"generator-star-spacing": "error",
-		// Disallow arrow functions where the could be confused with comparison
-		"no-confusing-arrow": "error",
 		// Disallow modifying variables named as class declarations
 		"no-class-assign": "error",
+		// Disallow arrow functions where the could be confused with comparison
+		"no-confusing-arrow": "error",
 		// Disallow modifying variables declared as constants
 		"no-const-assign": "error",
 		// Disallow duplicate named class members
 		"no-dupe-class-members": "error",
+		// Disallow duplicate imports, use a single import per module
+		"no-duplicate-imports": "error",
+		// Disallow symbol constructors
+		"no-new-symbol": "error",
+		// Disallow specific, restricted imports -- we should think about this list at some point.
+		"no-restricted-modules": "off",
 		// Disallow referencing this or super before super() call
 		"no-this-before-super": "error",
-		// Disallow use of var in favor of let and const. Need to discuss with team.
-		"no-var": "warn",
-		// Require object literal shorthand definition. Need to discuss with team.
-		"object-shorthand": "warn",
-		// Require use of arrow function notation in callbacks where appropriate. Need to discuss with team. Probably a good idea, but only in ES6 environments.
-		"prefer-arrow-callback": "warn",
-		// Require use of const when let or var assigned and never modified. Need to discuss with team. Probably a good idea, but only in ES6 environments.
-		"prefer-const": "warn",
-		// Require use of Reflect API where it can be used to replace older object methods. Needs configuration and need to discuss with team. Only applicable in ES6 environments
-		"prefer-reflect": "warn",
-		// Require the use of the spread operator instead of using Function.prototype.apply() function. Need to discuss with team. Probably a good idea, but only in ES6 environments.
-		"prefer-spread": "warn",
-		// Require use of template literals over string concatenation (e.g. var str = `Hello ${world}!`; rather than var str = "Hello " + world;). Need to discuss with team. Probably a good idea, but only in ES6 environments.
-		"prefer-template": "warn",
+		// Disallow unnecessary computerd property keys.
+		"no-useless-computed-key": "error",
+		// Disallow unnecessary constructors
+		"no-useless-constructor": "error",
+		// Disallow unnecessary renaming imports, exports and destructured assignments to the same name.
+		"no-useless-rename": "error",
+		// Disallow use of var in favor of let and const.
+		"no-var": "error",
+		// Require object literal shorthand definition.
+		"object-shorthand": ["error", "always"],
+		// Require use of arrow function notation in callbacks where appropriate.
+		"prefer-arrow-callback": "error",
+		// Require use of const when let or var assigned and never modified.
+		"prefer-const": "error",
+		// Require destructuring of objects and arrays.
+		"prefer-destructuring": "error",
+		// Disallow parseInt in favor of binary, octal, and hexadecimal literals
+		"prefer-numeric-literals": "error",
+		// Suggest using the rest parameters instead of arguments.
+		"prefer-rest-params": "error",
+		// Require the use of the spread operator instead of using Function.prototype.apply() function. 
+		"prefer-spread": "error",
+		// Require use of template literals over string concatenation (e.g. var str = `Hello ${world}!`; rather than var str = "Hello " + world;).
+		"prefer-template": "error",
 		// Require the use of yield expression in generator functions (without yield there is no reason to use a generator). Need to discuss with team. Probably a good idea, but only in ES6 environments
-		"require-yield": "warn"
+		"require-yield": "warn",
+		// Enforce spacing between rest and spread operators and their expressions
+		"rest-spread-spacing": ["error", "always"],
+		// Enforce sorting of imports.
+		"sort-imports": "error",
+		// Require symbol descriptions
+		"symbol-description": "error",
+		// Disallow spaces in template strings
+		"template-curly-spacing": ["error", "never"],
+		// Enforce spacing around yield expressions.
+		"yield-star-spacing": "error",
 	}
 };
